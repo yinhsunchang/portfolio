8931c398fe13ab247cf187ecbea45035083caeef
yinhsunchang <yinhsunchang@gmail.com>
2026-08-21 16:35:01 +0200

    docs: add skills

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,21 +2,26 @@ import { useTranslation } from "react-i18next";
 
 const About = () => {
   const { t } = useTranslation();
-  
-  return (
-  <div className="content justify text-grey padding-32" id="about">
-    <h2 className="text-dark-grey">{t("about.title")}</h2>
-    <hr className="opacity" />
-    <p>{t("about.text1")}</p>
-    <p>{t("about.text2")}</p>
-    
-    <h2 className="padding-top-64 text-dark-grey">{t("about.skills.title")}</h2>
-    <hr className="opacity" />
-    <p>{t("about.skills.frontend")}</p>
-    <p>{t("about.skills.design")}</p>
-    <p>{t("about.skills.language")}</p>
-  </div>
-  );
-}
 
-export default About
+  return (
+    <div className="content justify text-grey padding-32" id="about">
+      <h2 className="text-dark-grey">{t("about.title")}</h2>
+      <hr className="opacity" />
+      <p>{t("about.text1")}</p>
+      <p>{t("about.text2")}</p>
+
+      <h2 className="padding-top-64 text-dark-grey">
+        {t("about.skills.title")}
+      </h2>
+      <hr className="opacity" />
+      <p>{t("about.skills.frontend")}</p>
+      <p>{t("about.skills.backend")}</p>
+      <p>{t("about.skills.testing")}</p>
+      <p>{t("about.skills.tools")}</p>
+      <p>{t("about.skills.design")}</p>
+      <p>{t("about.skills.language")}</p>
+    </div>
+  );
+};
+
+export default About;
